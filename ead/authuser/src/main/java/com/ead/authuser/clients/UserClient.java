d72b2aa01f6a25be3b8c65f3728ff24dc52bff6c
alex.sousa <alex.sousa@sop.ce.gov.br>
2024-04-30 16:55:14 -0300

    chore: update

--- ead/authuser/src/main/java/com/ead/authuser/clients/UserClient.java
+++ ead/authuser/src/main/java/com/ead/authuser/clients/UserClient.java
@@ -1,13 +1,16 @@
 package com.ead.authuser.clients;
 
 import com.ead.authuser.dtos.CourseDto;
+import com.ead.authuser.dtos.ResponsePageDto;
 import lombok.extern.log4j.Log4j2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
@@ -32,6 +35,9 @@ public class UserClient {
         log.debug("Request  URL: {}", url);
         log.info("Request URL: {}", url);
         try {
+            ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType =
+                    new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {};
+            ResponseEntity<ResponsePageDto<CourseDto>> result = restTemplate.
             log.debug("Response Number of Elements: {}", searchResult.size());
         }catch (HttpStatusCodeException e){
             log.error("Error request / courses {}", e);
